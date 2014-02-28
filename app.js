81e050eb548b574af0c08687e0361987f6171687
Jkohli <jkohli@g2ft.com>
2014-02-28 17:40:09 -0500

    botched redis setup

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (process.env.REDISTOGO_URL) {
     var rtg = require("url").parse(process.env.REDISTOGO_URL);
 	var redis = require("redis").createClient(rtg.port, rtg.hostname);
 
-	redis.redis.auth(rtg.auth.split(":")[1]);
+	redis.client.auth(rtg.auth.split(":")[1]);
 	
 } else {
     var redis = require("redis").createClient();
